d4641dd2bcc0f39fe292930116b737aba28eefdb
NguyenMauAnh <twas.horus@gmail.com>
2024-08-09 14:08:04 +0700

    Update GlobalExceptionHandler to fix error when build

--- starter_code/src/main/java/com/example/demo/config/GlobalExceptionHandler.java
+++ starter_code/src/main/java/com/example/demo/config/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.example.demo.config;
 import com.example.demo.controllers.UserController;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.omg.CORBA.portable.ApplicationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
